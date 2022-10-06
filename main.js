4f53786196093e77947ab4232d75158af9d646c2
Temple Igwe <temple.igw@gmail.com>
2022-10-06 16:50:29 +0100

    Add background color to form element

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-let myLibrary = [];
+let myLibrary = [{
+    title: 'Glory',
+    author:'Novilet Bulawayo',
+    pages:576,
+    read: 'false'
+},{
+    title: 'Things We Never Got Over',
+    author:'Lucy Score',
+    pages:384,
+    read: 'false'
+},{
+    title: 'Verity',
+    author:'Colleen Hoover',
+    pages:336,
+    read: 'false'
+},{
+    title: 'Righteous Prey',
+    author:'John Sandford',
+    pages:412,
+    read: 'false'
+}
+];
+
 
 function Book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-    this.info = function(){
-        return (title + author + ", " + pages + ","  + read)
-    }
+   
 }
 
-const theHobbit = new Book("The Hobbit", "by J.R.R. Tolkein", "295 pages", "not read yet")
-console.log(theHobbit.info());
+function addBookToLibrary(title, author, pages, read){
+    let book = new Book (title, author, pages, read);
+    myLibrary.push(book);
+
+}
+
+
+function bookDisplay(){
+    const books = document.querySelector('.books');
+
+    myLibrary.forEach((myLibrary) => {
+
+    })
+}

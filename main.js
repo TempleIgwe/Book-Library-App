f0cdf8f870379e0f07425fff839c79845cd96142
TempleIgwe <temple.igw@gmail.com>
2022-10-10 05:19:06 +0100

    Add book image source

--- main.js
+++ main.js
@@ -1,33 +1,41 @@
-let openButton = document.body.querySelector(".openform")
+let myLibrary = [];
+let openButton = document.body.querySelector(".openform");
 openButton.addEventListener("click", function(){
     document.querySelector(".popup").style.display = "flex";
-})
-// let bookDetails = document.getElementById("formSubmit", bookCard());
-let submitForm = document.getElementById("formSubmit").innerHTML;
-submitForm.addEventListener("click", formData()).innerHTML;
+}) 
+ 
+// let submit = document.querySelector(".formSubmit");
 
-// let myLibrary = [{
-//     title: 'Glory',
-//     author:'Novilet Bulawayo',
-//     pages:576,
-//     read: false
-// },{
-//     title: 'Things We Never Got Over',
-//     author:'Lucy Score',
-//     pages:384,
-//     read: false
-// },{
-//     title: 'Verity',
-//     author:'Colleen Hoover',
-//     pages:336,
-//     read: false
-// },{
-//     title: 'Righteous Prey',
-//     author:'John Sandford',
-//     pages:412,
-//     read: false
+
+function bookDisplay(){
+    let title = document.querySelector("#formTitle");
+    let author = document.querySelector("#formAuthor");
+    let pages = document.querySelector("#formPages");
+    let bookImage = document.querySelector("#formImage");
+    let read = document.querySelector("#formRead");
+    let bookCard = document.querySelector(".books");
+
+    bookCard.innerHTML = title.value + author.value + pages.value + bookImage.value + read.value;
+
+}
+
+
+// let bookDetails = document.getElementById("formSubmit", bookCard());
+// let submitForm = document.getElementById("formSubmit");
+// submitForm.addEventListener("click", createElement()).innerHTML;
+
+// function createElement(){
+
 // }
-// ];
+
+
+// let books = document.createElement("h1");
+// document.body.appendChild(books);
+// books.style.textAlign = "center";
+
+// let submit = formData();
+
+
 
 
 function Book(title, author, pages, bookImage,read){
@@ -43,22 +51,24 @@ function Book(title, author, pages, bookImage,read){
 function addBookToLibrary(title, author, pages, bookImage, read){
     let book = new Book (title, author, pages,  bookImage, read);
     myLibrary.push(book);
-
-}
-function formData(){
     
-    let title = document.getElementById("formTitle").value;
-    let author = document.getElementById("formAuthor").value;
-    let pages = document.getElementById("formAuthor").value;
-    let bookImage = document.getElementById("formImage").value;
-    let read = document.getElementById("formRead").value;
+}
+// function formData(){
     
-    return formData(title, author, pages, bookImage, read);
+//     let title = document.querySelector("#formTitle").value;
+//     document.getElementById("formAuthor").value;
+//     document.getElementById("formImage").value;
+//     document.getElementById("formRead").value;
+    
+    
 
-}
-function bookCard(){
-    document.querySelector(".books").innerHTML = submitForm;
-}
+// }
+// function bookCard(){
+//     document.querySelector(".books").innerHTML = submitForm;
+// }
+// function createElement(){
+//     let card = document.formData("div")
+// }
 
 
 // function bookDisplay(){

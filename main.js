13a29b998dffa2bc092da6f1033f5a495cee4aa9
Temple Igwe <temple.igw@gmail.com>
2022-10-11 10:50:10 +0100

    Add book display function

--- main.js
+++ main.js
@@ -3,81 +3,52 @@ let openButton = document.body.querySelector(".openform");
 openButton.addEventListener("click", function(){
     document.querySelector(".popup").style.display = "flex";
 }) 
- 
-// let submit = document.querySelector(".formSubmit");
-
-
-function bookDisplay(){
-    let title = document.querySelector("#formTitle");
-    let author = document.querySelector("#formAuthor");
-    let pages = document.querySelector("#formPages");
-    let bookImage = document.querySelector("#formImage");
-    let read = document.querySelector("#formRead");
-    let bookCard = document.querySelector(".books");
-
-    bookCard.innerHTML = title.value + author.value + pages.value + bookImage.value + read.value;
-
-}
-
-
-// let bookDetails = document.getElementById("formSubmit", bookCard());
-// let submitForm = document.getElementById("formSubmit");
-// submitForm.addEventListener("click", createElement()).innerHTML;
-
-// function createElement(){
-
-// }
-
-
-// let books = document.createElement("h1");
-// document.body.appendChild(books);
-// books.style.textAlign = "center";
-
-// let submit = formData();
-
-
-
 
 function Book(title, author, pages, bookImage,read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.bookImage = bookImage;
-    this.read = read;
-    
+    this.read;
+}
+
+
+function bookDisplay(){
+   myLibrary.forEach(book => {
+
+       let bookCard = document.body.querySelector(".books");
+       let bookElement = `<li>
+       <h1>${book.title}</h1>
+       <h1>${book.author}</h1>
+       <h1>${book.pages}</h1>
    
+       </li>`
+       bookCard.innerHTML += bookElement
+    //    bookCard.append()
+       
+    })
 }
 
 function addBookToLibrary(title, author, pages, bookImage, read){
-    let book = new Book (title, author, pages,  bookImage, read);
+    let book = new Book(title, author, pages,  bookImage, read);
     myLibrary.push(book);
-    
 }
-// function formData(){
-    
-//     let title = document.querySelector("#formTitle").value;
-//     document.getElementById("formAuthor").value;
-//     document.getElementById("formImage").value;
-//     document.getElementById("formRead").value;
-    
-    
 
-// }
-// function bookCard(){
-//     document.querySelector(".books").innerHTML = submitForm;
-// }
-// function createElement(){
-//     let card = document.formData("div")
-// }
+function handleSubmit(e){
+    e.preventDefault()
+    let title = document.querySelector("#formTitle").value;
+    let author = document.querySelector("#formAuthor").value;
+    let pages = document.querySelector("#formPages").value;
+    let bookImage = document.querySelector("#formImage").value;
+    let read = document.querySelector("#form-Read").value;
+    let notRead = document.querySelector("#form-NotRead").value;
+    addBookToLibrary(title, author, pages, bookImage, read || !notRead);
+    bookDisplay()
+}
+ 
+let submit = document.body.querySelector(".formSubmit");
+submit.addEventListener("click", handleSubmit);
 
 
-// function bookDisplay(){
-//     const books = document.querySelector('.books');
 
-//     myLibrary.forEach((myLibrary) => {
-//         for(const i=0; i < myLibrary.length; i++){
-//             console.log(myLibrary)
-//         }
-//     })
-// }
 

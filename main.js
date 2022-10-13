66de724acbfebb3bdf2bd4b79816f465ffcd9ab2
Temple Igwe <temple.igw@gmail.com>
2022-10-13 12:47:55 +0100

    Add reset method for form

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-let myLibrary = [];
-let openButton = document.body.querySelector(".openform");
-openButton.addEventListener("click", function(){
+/* eslint-disable*/
+const myLibrary = [];
+const openButton = document.body.querySelector('.openform');
+openButton.addEventListener('click', function(){
     document.querySelector(".popup").style.display = "flex";
-}) 
+})
 
-function Book(title, author, pages, bookImage,isRead){
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
+function Book(title, author, pages, bookImage,isRead) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
     this.bookImage = bookImage;
     this.isRead = isRead;
 }
@@ -22,8 +23,7 @@ function bookDisplay(){
        <h3>${book.author}</h3>
        <h3>${book.pages}</h3>
        <img src = ${book.image}/>
-       <h3>${book.isRead}</h3>
-
+       <h3>${book.isRead} || ${book.notRead}</h3>
        `
        bookCard.innerHTML += bookElement
     //    bookCard.append()
@@ -44,8 +44,14 @@ function handleSubmit(e){
     let bookImage = document.querySelector("#formImage").value;
     let read = document.querySelector("#form-Read").value;
     let notRead = document.querySelector("#form-NotRead").value;
+
+   
+    
     addBookToLibrary(title, author, pages, bookImage, read || !notRead);
     bookDisplay()
+
+    document.querySelector(".popup").reset();
+    
 }
  
 let submit = document.body.querySelector(".formSubmit");
